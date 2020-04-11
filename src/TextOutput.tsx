779d99fb64faa6a324988bfeecb488b93ba62ae8
Karan Hudia <khudia@goodgamestudios.com>
2020-04-11 13:08:29 +0200

    retain formatting

--- src/TextOutput.tsx
+++ src/TextOutput.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const TextOutput = ({text}) => {
     return (
-        <div className="text-output">
-            {text}
-        </div>
+        <textarea className="text-output" value={text} />
     );
 };
 
