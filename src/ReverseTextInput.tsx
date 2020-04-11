7546037d60ee973b8bd053f4574b25bcc392e678
Karan Hudia <khudia@goodgamestudios.com>
2020-04-11 12:43:42 +0200

    Show off the count

--- src/ReverseTextInput.tsx
+++ src/ReverseTextInput.tsx
@@ -3,20 +3,24 @@ import React from 'react';
 const ReverseTextInput = ({onChange}) => {
     const handleChange = (e) => {
         const text = e.target.value;
+        let count = 0;
 
         const regex1 = /([a-zA-Z]+)\s{1}([A-Z]+),/gm;
         const regex2 = /,\s([a-zA-Z]+)\s([A-Z]+)\s(\(\d{4}[a-z]?\))/gm;
         const regex3 = /(\(\d{4}\))\s/gm;
-        // const regex4 = /\s(\\([0-9]-?[0-9]?\\))/gm;
 
-        // const regexMatch1 = text.match(regex1);
-        // let output = `${regexMatch1[1]}, ${regexMatch1[2].split('').join('. ')}`;
+        count += text.match(regex1).length;
         let output = text.replace(regex1, (match, firstName, lastName) => `${firstName}, ${lastName.split('').join('. ')}.,`);
+
+        count +=output.match(regex2).length;
         output = output.replace(regex2, (match, firstName, lastName, year) => ` & ${firstName}, ${lastName.split('').join('. ')}. ${year}`);
+
+        count +=output.match(regex3).length;
         output = output.replace(regex3, "$1: ");
-        // output = output.replace(regex4, "$1");
 
         onChange(output)
+
+        alert(`Karan Hudia just helped you save ${count} matches and corrections.`)
     };
 
     return (
